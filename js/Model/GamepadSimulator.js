e654a5720094bb4b8cf7239c16f9c2e8b0d49488
hutchings90 <hutchings90@gmail.com>
2019-01-31 20:36:32 -0700

    Updates GamepadSimulator button and adds comments.

--- js/Model/GamepadSimulator.js
+++ js/Model/GamepadSimulator.js
@@ -1,3 +1,15 @@
+/**
+ * @file
+ *
+ * This simulates a gamepad. A gamepad (for our purposes) consists of 10
+ * buttons and 2 axes. The axes have a value from -1 to 1 (inclusive). The
+ * buttons are objects with only one key. The key is a boolean called
+ * pressed. Each frame, pressed is checked and interpreted by the
+ * GamepadReader, GamepadStatus, GamepadAxisStatus, GamepadButtonStatus
+ * and GamepadItemStatus. The possible interpretations are start, end, idle
+ * repeat, prepeat and postpeat.
+ */
+
 function GamepadSimulator() {
 	// console.log('GamepadSimulator');
 	this.buttons = [ this.button(), this.button(), this.button(), this.button(), this.button(), this.button(), this.button(), this.button(), this.button(), this.button(), this.button(), this.button() ];
@@ -6,7 +18,6 @@ function GamepadSimulator() {
 
 GamepadSimulator.prototype.button = function() {
 	return {
-		pressed: false,
-		value: 0
+		pressed: false
 	};
 };
