70d9f60af52278011a74799fe883bd366847b29c
Antman537 <32137706+Antman537@users.noreply.github.com>
2019-02-06 14:26:06 -0700

    laid out functions for overworldController

--- js/Controller/OverworldController.js
+++ js/Controller/OverworldController.js
@@ -7,3 +7,21 @@ function OverworldController(model, view, utils, contentManager) {
 
 OverworldController.prototype = Object.create(GamepadProcessingController.prototype);
 OverworldController.constructor = OverworldController;
+
+OverworldController.prototype.startBattle = function(params){};
+
+OverworldController.prototype.interact = function(params){};
+
+OverworldController.prototype.left = function (params){};
+
+OverworldController.prototype.right = function (params){};
+
+OverworldController.prototype.up = function (params){};
+
+OverworldController.prototype.down = function (params){};
+
+/*
+The map information will need to be stored and processed somehow. 
+This function loads and manages the map info
+*/
+OverworldController.prototype.loadMap = function (params){};
