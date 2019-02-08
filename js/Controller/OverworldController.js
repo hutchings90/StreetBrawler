667227d40bde8796c3a2514ff2fe8703f6319da8
Antman537 <32137706+Antman537@users.noreply.github.com>
2019-02-08 14:19:21 -0700

    OverworldCharacter
    Created OverworldCharacter model and began integrating it into overworldController.

--- js/Controller/OverworldController.js
+++ js/Controller/OverworldController.js
@@ -3,22 +3,80 @@ function OverworldController(model, view, utils, contentManager) {
 	GamepadProcessingController.call(this, view, utils, contentManager);
 	utils.makeControllerVariableInput(this, model);
 	this.overworld = this.view.getOverworld();
+	//TODO: attach corect OverWorld character
 }
 
 OverworldController.prototype = Object.create(GamepadProcessingController.prototype);
 OverworldController.constructor = OverworldController;
 
+OverworldController.prototype.nextFrame = function(inputs) {
+	// console.log('BattleCharacterController');
+	for (var i = inputs.length - 1; i >= 0; i--) {
+		var input = inputs[i];
+		var status = input.status;
+		if (!status) continue;
+		var character = this.characters[i];
+		if (this.buttonPressed(status.buttons[9])) return this.createReport('showBattleMenu', {}, input.pi);
+		this.processAxes(character, status.axes);
+		this.processButtons(character, status.buttons);
+	}
+
+	for (var i = this.characters.length - 1; i >= 0; i--) {
+		var character = this.characters[i];
+		character.character.move();
+		this.view.setCharacterPosition(character);
+	}
+};
+
 OverworldController.prototype.startBattle = function(params){};
 
 OverworldController.prototype.interact = function(params){};
 
-OverworldController.prototype.left = function (params){};
+OverworldController.prototype.left = function (character){
+	character.character.left();
+};
 
-OverworldController.prototype.right = function (params){};
+OverworldController.prototype.right = function (character){
+	character.character.right();
+};
 
-OverworldController.prototype.up = function (params){};
+OverworldController.prototype.up = function (character){
+	character.character.up();
+};
 
-OverworldController.prototype.down = function (params){};
+OverworldController.prototype.down = function (character){
+	character.character.down();
+};
+
+BattleCharacterController.prototype.processAxes = function(character, axes) {
+	// console.log('processAxes');
+	this.processAction(character, this[character.character.state + 'Axes'](axes));
+};
+
+BattleCharacterController.prototype.idleAxes = function(axes) {
+	// console.log('idleAxes');
+	var v = this.verticalDirection(axes);
+	var h = this.horizontalDirection(axes);
+	if (v == 1) return this.createReport('down');
+	if (v == -1) return this.createReport('up', {
+		direction: h
+	});
+	if (h == 1) return this.createReport('right');
+	if (h == -1) return this.createReport('left');
+};
+
+BattleCharacterController.prototype.walkAxes = function(axes) {
+	// console.log('walkAxes');
+	var v = this.verticalDirection(axes);
+	var h = this.horizontalDirection(axes);
+	if (v == 1) return this.createReport('down');
+	if (v == -1) return this.createReport('up', {
+		direction: h
+	});
+	if (h == 1) return this.createReport('right');
+	if (h == -1) return this.createReport('left');
+	return this.createReport('resetState');
+};
 
 /*
 The map information will need to be stored and processed somehow. 
---
+++ js/Model/OverworldCharacter.js
@@ -0,0 +1,91 @@
+function OverworldCharacter(name) {
+	// console.log('Character');
+	//TODO: update vars
+	//this.GRAVITY = -.1;
+	//this.JUMP_VY = 20;
+	//this.JUMP_VX = 6;
+	this.WALK_V = 2;
+	this.BATTLE_AREA_W = 900;
+	this.BATTLE_CHARACTER_W = 90;
+	this.MAX_X = this.BATTLE_AREA_W - this.BATTLE_CHARACTER_W;
+	this.name = name;
+	this.health = 100;
+	this.x = 0;
+	this.y = 0;
+	this.dx = 0;
+	this.dy = 0;
+	//this.jumpFrames = 0;
+	this.resetState();
+}
+
+OverworldCharacter.prototype.reset = function(x) {
+	// console.log('reset');
+	this.x = x || 0;
+	this.y = 0;
+	this.dx = 0;
+	this.dy = 0;
+};
+
+OverworldCharacter.prototype.resetState = function() {
+	console.log('resetState');
+	this.state = 'idle';
+	this.move = this.walkMove;
+	this.reset(this.x);
+};
+
+OverworldCharacter.prototype.left = function() {
+	// console.log('left');
+	this.walk(-1,0);
+};
+
+OverworldCharacter.prototype.right = function() {
+	// console.log('right');
+	this.walk(1,0);
+};
+
+OverworldCharacter.prototype.up = function() {
+	this.walk(0,1)
+};
+
+OverworldCharacter.prototype.down = function() {
+	
+};
+
+OverworldCharacter.prototype.walk = function(directionX, directionY) {
+	// console.log('walk');
+	this.state = 'walk';
+	this.dx = directionX * this.WALK_V;
+	this.dy = directionY * this.WALK_V;
+};
+
+
+
+OverworldCharacter.prototype.walkMove = function() {
+	// console.log('move');
+	this.moveX();
+	this.moveY();
+};
+
+OverworldCharacter.prototype.moveX = function() {
+	// console.log('moveX');
+	this.setX(this.dx + this.x);
+};
+
+OverworldCharacter.prototype.moveY = function() {
+	// console.log('moveY');
+	this.setY(this.dy + this.y);
+};
+
+OverworldCharacter.prototype.setX = function(x) {
+	// console.log('setX');
+	if (x < 0) x = 0;
+	if (x > this.MAX_X) x = this.MAX_X;
+	this.x = x;
+};
+
+OverworldCharacter.prototype.setY = function(y) {
+	// console.log('setY');
+	//TODO: 
+	if (y < 0) y = 0;
+	this.y = y;
+};
