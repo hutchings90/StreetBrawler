7b6d6d7c5207b26f63c03275bcab40cea6d0e0c2
Nathan Geronimo <nathangero@yahoo.com>
2019-03-08 01:22:28 -0700

    Made changes to AIPlayer and AIController so the AI can act on its own.

--- js/Controller/AIPlayerController.js
+++ js/Controller/AIPlayerController.js
@@ -23,9 +23,8 @@ AIController.prototype.activate = function() {
 	// this does not refer to this instance of AIController in the inverval
 	var me = this;
 	me.ai.playing = true;
-	me.interval = setInterval(function() {
-		// TODO: Implement AIPlayer decision making.
-		me.makeDecisions();
+	me.interval = setInterval(function() { // IPlayer decision making.
+		me.makeRandomDecision();
 	}, me.ai.getDifficultyFrequency());
 };
 
@@ -42,34 +41,49 @@ AIController.prototype.deactivate = function() {
 
 /**
  * private
- * Determines which buttons/axes should be pressed/released
+ * Determines which buttons/axes should be pressed/released.
+ * 
+ * Since the AI makes a decision depending on the amount of time passed instead of frames,
+ * we have to consider that every button/axis pressed will be pressed for a certain amount of time.
+ * So far, jumping, grabbing, and attacking (all moves that should only execute ONCE per button press),
+ * will be released the very next time this funciton is called. That way the AI isn't always jumping 
+ * until the AI can make its next decision.
  */
-AIController.prototype.makeDecisions = function() {
-	console.log('makeDecisions');
-	this.makeRandomDecision();
-}
-
 AIController.prototype.makeRandomDecision = function() {
-	console.log('makeRandomDecision');
 	var gamepad = this.ai.gamepadReader.gamepad;
-	gamepad.clear();
-	// 50% chance that a button will be pressed
-	if (Math.floor((Math.random() * 2)) == 1) {
-		// The last four buttons of a GamepadSimulator are there to
-		//   match the layout of a real gamepad. They should not be
-		//   used by the AIPlayer.
-		gamepad.pressButton(Math.floor(Math.random() * (gamepad.buttons.length - 4)));
+	var pickHorizontalMovement = Math.floor(Math.random() * Math.floor(2)); // Picks a number between 0 and 1
+	// var pickVerticalMovement = Math.floor(Math.random() * Math.floor(2)); // Picks a number between 0 and 1
+	var flipToNeg = Math.floor(Math.random() * Math.floor(2)); // Picks a number between 0 and 1
+	var jumpOrWalk = Math.floor(Math.random() * Math.floor(2)); // Picks a number between 0 and 1
+
+	if (flipToNeg == 0) { pickHorizontalMovement *= -1; }
+	// else if (flipToNeg == 1) { pickVerticalMovement *= -1; }
+
+	if (this.ai.isJumping) { // Only make the AI jump once for one button input
+		gamepad.releaseAxis(VERTICLE_AXIS); // Stop moving
+		this.ai.toggleJumping();
+		jumpOrWalk = 0; // If the AI just jumped, don't let it jump again but make it walk if it does an action.
 	}
-	else {
-		// 50% chance that the horizontal axis will be pressed
-		if (Math.floor((Math.random() * 2)) == 1) {
-			// 50/50 left/right
-			gamepad.pressAxis(0, Math.floor((Math.random() * 2)) == 1 ? -1 : 1);
+	if (this.ai.canDoMove()) {
+		if (this.ai.isWalking) {
+			gamepad.releaseAxis(HORIZONTAL_AXIS); // Stop moving
+			this.ai.toggleWalking();
 		}
-		// 50% chance that the vertical axis will be pressed
-		if (Math.floor((Math.random() * 2)) == 1) {
-			// 50/50 up/down
-			gamepad.pressAxis(1, Math.floor((Math.random() * 2)) == 1 ? -1 : 1);
+		else if (this.ai.isJumping) {
+			gamepad.releaseAxis(VERTICLE_AXIS); // Stop moving
+			this.ai.toggleJumping();
 		}
+		else { // If the AI isn't doing anything, make it do something
+			if (jumpOrWalk == 0) { // AI walks
+				gamepad.pressAxis(HORIZONTAL_AXIS, pickHorizontalMovement);
+				this.ai.toggleWalking();
+			}
+			else { // AI jumps
+				gamepad.pressAxis(VERTICLE_AXIS, MOVE_JUMP);
+				this.ai.toggleJumping();
+			}
+		}
+		this.ai.resetFrequency(); // Always reset the frequency when an action is done
 	}
+	this.ai.incrementFrequency(); // Always increment the frequency
 };
